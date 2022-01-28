309467c571596d96c53bf97aef376b9bcdea4ca7
adelpro <adelpro@gmail.com>
2022-01-28 22:06:39 +0100

    about multi language working

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,27 @@
 import { Fragment } from "react";
 import Footer from "../components/Footer";
+import { useContext } from "react";
+import { AdhanParamContext } from "../context/AdhanParamContext";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 export default function About() {
+  const { lan: language } = useContext(AdhanParamContext);
+  const content = {
+    English: {
+      about:
+        "OpenAdhan allaw remember you about prayer times directly in the browser",
+    },
+    Arabic: {
+      about: "تطبيق أذان يذكرك بأوقات الصلاة مباشرة من المتصفح.",
+    },
+  };
   return (
     <Fragment>
-      <p>About</p>
+      <Box mt={8} mb={8}>
+        <Typography variant="body1" textAlign="center">
+          {content[`${language}`][`about`]}
+        </Typography>
+      </Box>
       <Footer />
     </Fragment>
   );
